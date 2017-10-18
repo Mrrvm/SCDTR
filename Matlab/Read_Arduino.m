99928b4d97550414c8c1ccd042c0835f2274bf0e
Filmad <Filmad@users.noreply.github.com>
2017-10-18 23:20:36 +0100

    Novo codigo

--- Matlab/Read_Arduino.m
+++ Matlab/Read_Arduino.m
@@ -1,17 +1,21 @@
 clear all
 clc
-
-arduino=serial('COM10','BaudRate',9600);
-
-%if(fopen(arduino) ~= -1)
+j = 0;
+f = 0;
+i = 1;
+arduino=serial('COM3','BaudRate',9600);
 fopen(arduino);
-    x=linspace(0,25*50*20,25*50);
 
-    for(i=1:length(x))
-        y(i)=fscanf(arduino,'%f');
-    end
-    fclose(arduino);
-    plot(x,y);
-%end
-
+ while i>0
+     y(i)=fscanf(arduino,'%f');
+     if (y(i) == -1)
+         break
+     end
+     i = i +1;
+ end
+ tempo = fscanf(arduino,'%f');
+ x=linspace(0,tempo,(i-1));
+ fclose(arduino);
+ y = y(1:i-1);
+ plot(x,y);
 
