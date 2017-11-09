c11092f9b199826ce042664cbc74a6d54c33fa35
Mrrvm <mrrvm@hotmail.com>
2017-11-09 19:40:07 +0000

    Broadcast is now working

--- Arduino/Arduino_I2C/Arduino_I2C.ino
+++ Arduino/Arduino_I2C/Arduino_I2C.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 
+#define MY_ADDRESS 1 // change this
 
 void receiveEvent(int howMany)	{
   while(Wire.available() >	0)	{
@@ -10,7 +11,8 @@ void receiveEvent(int howMany)	{
 
 void setup() {
   Serial.begin(9600);  
-  Wire.begin(1);
+  Wire.begin(MY_ADDRESS);
+  TWAR = (MY_ADDRESS << 1) | 1;
   Wire.onReceive(receiveEvent);
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
@@ -21,7 +23,7 @@ void loop() {
   char c;
   if(Serial.available()>0)	{
     c=Serial.read();
-    Wire.beginTransmission(0);	
+    Wire.beginTransmission(0);	// broadcast to all
     Wire.write(c);	
     Wire.endTransmission();
   }
